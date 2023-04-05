bc2ec639bd32d3ea047be63a7aab07e9e3f7c273
fzero17 <wshutalk@gmail.com>
2023-04-05 11:45:41 +0800

    update info

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -23,14 +23,16 @@ const onDataUrlChange = (base64Url) => {
     canvas.height = 400
     // 绘制图片
     ctx.drawImage(img, 0, 0, width, height)
-    // 绘制文字
+
+    // 绘制网址
     ctx.font = 'bold 14px Arial'
     ctx.fillStyle = 'blue'
     ctx.fillText(qrCodeString.value, 0, height + 20)
-    ctx.font = 'bold 20px Arial'
+
+    ctx.font = 'bold 14px Arial'
     ctx.fillStyle = 'red'
     ctx.fillText(title.value, 0, height + 40)
-    ctx.font = 'bold 16px Arial'
+    ctx.font = 'bold 14px Arial'
     ctx.fillText(subTitle.value, 0, height + 60)
   }
 }
@@ -57,10 +59,10 @@ const onTextChange = () => {
     </div>
     <div class="flex flex-col w-1/2 pt-5">
       <label class="font-light" for="text">文本</label>
-      <textarea id="text" class="border border-b-gray-300" v-model="qrCodeString"/>
-      <label for="title" class="font-light">一级标题</label>
+      <textarea id="text" class="border border-b-gray-300 h-40" v-model="qrCodeString"/>
+      <label for="title" class="font-light">备注一</label>
       <input id="title" class="border border-b-gray-300" v-model="title" @input="onTextChange"/>
-      <label for="subTitle" class="font-light">二级标题</label>
+      <label for="subTitle" class="font-light">备注二</label>
       <input id="subTitle" class="border border-b-gray-300" v-model="subTitle" @input="onTextChange"/>
     </div>
   </div>
